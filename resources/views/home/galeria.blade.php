e990122898e4b08f6e1ac499111880ed96e9ac8a
Fabio Diaz <fabioedp15@gmail.com>
2017-10-01 22:15:11 -0500

    galeria

--- resources/views/home/galeria.blade.php
+++ resources/views/home/galeria.blade.php
@@ -1,34 +1,80 @@
 @extends('home.template')
 @section('content')
 
-<div class="container">
-  <div class="panel-heading">
+extends('home.template')
+@section('content')
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Galeria</title>
+  </head>
+  <body>
+    <section class="container">
       <div class="row">
 
-          <div class="col-xs-6 col">
-              <a href="" class="active" id="login-form-link">Fotos</a>
-<div class="galeria-fotos">
-<ul>
-  <li></li>
-</ul>
-</div>
-
+        <div class="col-md-7">
+          <div class="galeria">
+              <h2>Galería</h2>
           </div>
-          <div class="col-xs-6">
-              <a href="#" id="register-form-link">Videos</a>
-              <div class="galeria-videos">
-<ul>
-  <li></li>
-</ul>
-              </div>
+          <div class="row">
 
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso2.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso3.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso4.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso5.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso2.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 descripcion">
+               <img src="img/img-acoso3.jpeg" class="img-responsive">
+               <p>Campaña contra el acoso callejero</p>
+            </div>
           </div>
+        </div>
+
+        <div class="col-md-5 v-descripcion">
+          <div class="galeria">
+              <h2>Videos</h2>
+          </div>
+            <img src="img/img-acoso.jpeg" class="img-responsive">
+            <p>Campaña contra el acosos callejero</p>
+            <img src="img/img-acoso2.jpeg" class="img-responsive">
+            <p>Campaña contra el acosos callejero</p>
+            <img src="img/img-acoso3.jpeg" class="img-responsive">
+            <p>Campaña contra el acosos callejero</p>
+          </div>
+
       </div>
-      <hr>
-  </div>
+    </section>
 
-</div>
+  </body>
+</html>
+
+
+@stop
 
 
 
