5b085cd322a29c9c076e943878ac88ff940aa426
dotiucr <32272918+DotiU@users.noreply.github.com>
2017-10-15 19:38:36 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '185.176.43.49'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', '2445951_app'),
-            'username' => env('DB_USERNAME', '2445951_app'),
-            'password' => env('DB_PASSWORD', 'znay1094'),
+            'database' => env('DB_DATABASE', 'dotiu'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'latin1_swedish_ci',
